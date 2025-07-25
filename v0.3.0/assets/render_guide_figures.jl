38f1a7a3a6867d9326679b272870b5e863f9dc30
Documenter.jl <documenter@juliadocs.github.io>
2025-07-25 15:41:29 +0000

    build based on 5309811

---
+++ v0.3.0/assets/render_guide_figures.jl
@@ -0,0 +1,15 @@
+using ComplexToys
+
+save("phase-id.png", domaincolor(z->z))
+save("phase-pole.png", domaincolor(z->z^-2))
+save("box-1.png", domaincolor(z->z, 2; box=(.4,.6+.2im,:white)))
+save("box-2.png", domaincolor(z->1/conj(z), 2; box=(.4,.6+.2im,:white)))
+save("box-3.png", domaincolor(z->1/z, 2; box=(.4,.6+.2im,:white)))
+save("candp.png", domaincolor(z -> (conj(z) - .5)/(z + .5), abs=true))
+save("ess.png", domaincolor(z -> exp(1/z), .2, abs=Inf))
+save("sin-ess.png", modularsurface(z -> sin(1/z)))
+save("dc-pow2.png", domaincolor(z->z^2, abs=true))
+save("bc-sqrt.png", domaincolor(z->z^(1/2), abs=true))
+save("rm-sqrt.png", riemannpow(1//2))
+save("rm-log.png", riemannlog())
+save("rms-sin.png", riemannsphere(sin))
